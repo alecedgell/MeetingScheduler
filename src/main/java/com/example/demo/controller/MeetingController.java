71ac708ef2008b369cc2a6041edfe03809a6b067
Noah Plautz <plautznl4327@uwec.edu>
2020-10-27 14:42:07 -0500

    added table to display from db

--- src/main/java/com/example/demo/controller/MeetingController.java
+++ src/main/java/com/example/demo/controller/MeetingController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 public class MeetingController {
 
     @Autowired
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -7,7 +7,7 @@ import javax.persistence.Table;
 
 
 @Entity
-@Table(name = "all_user")
+@Table
 
 public class User {
 
